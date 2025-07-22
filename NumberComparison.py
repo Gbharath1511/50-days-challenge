4ba06bc3a25018ed7b854877754df0b7d3403afb
Gbharath1511 <gbharath.ai2025@gmail.com>
2025-07-23 05:22:55 +0530

    Create NumberComparison.py

---
+++ NumberComparison.py
@@ -0,0 +1,13 @@
+# Compare Two Numbers
+
+# Get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Compare the numbers
+if num1 > num2:
+    print(f"{num1} is greater than {num2}.")
+elif num1 < num2:
+    print(f"{num1} is less than {num2}.")
+else:
+    print(f"Both numbers are equal.")
